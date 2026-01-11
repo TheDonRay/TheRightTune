f3a37424aa9faa06a075e933015bb77fbc5b011f
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-11 13:34:15 -0500

    setting up this projec

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config(); 
+const app = require('./app.js'); 
+
+const PORT = process.env.PORT; 
+
+app.listen(PORT, () => { 
+    console.log(`Server Successfully running on http://localhost:${PORT}`)
+}); 
+
