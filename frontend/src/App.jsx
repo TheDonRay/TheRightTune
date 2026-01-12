1225395239862b23c16a9f44c9430da12816f3c7
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-12 17:55:04 -0500

    working on the redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import "./App.css";
+import "./App.css"; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Homepage from "./Components/homepage.jsx";
 function App() {
   return (
