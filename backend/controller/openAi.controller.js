713ff116f2fab5db18b9458250a3e72ea68827d4
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-12 17:15:26 -0500

    working on some backend code here

--- backend/controller/openAi.controller.js
+++ backend/controller/openAi.controller.js
@@ -1,4 +1,23 @@
+//Call the openAi library etc 
 
-// const sendtodjAI = async () => { 
-//     const {} = req.body; 
-// } 
+//remeber the SDLC : 
+/*planning, analysis, design, implementation, testing and integration, maintanence. */
+const aidj = async (req, res) => { 
+    // implement try and catch case 
+    // for now just for testing purposes we can start of with   
+    // remember get requests should not have body params, so like waht you had before it should not have that req.body from the mood 
+    try { 
+        const { mood } = req.query;  
+        res.status(200).json({ 
+            backendResponse: mood
+        }); 
+        console.log('backend said: ', mood); 
+
+    } catch (error) { 
+        res.status(500).json({ 
+            backendError: 'Nothing recieved in the backend'
+        }); 
+    } 
+} 
+
+module.exports = aidj; 
